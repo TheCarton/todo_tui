7a1df46973ed7093952874486a22b4ad7ba35d24
TheCarton <theukearchy@gmail.com>
2024-09-08 12:50:01 -0400

    blocks look gud

--- src/task.rs
+++ src/task.rs
@@ -3,7 +3,7 @@ use std::iter::{zip, Chain};
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Direction, Layout, Rect},
-    widgets::{Paragraph, Widget},
+    widgets::{Block, Paragraph, Widget},
 };
 use time::OffsetDateTime;
 
@@ -25,19 +25,21 @@ pub enum TaskStatus {
 impl Widget for Task {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let date_added = Paragraph::new(format!(
-            "added: {}:{}:{} {}",
+            "{}:{}:{} {}",
             self.time_added.hour(),
             self.time_added.minute(),
             self.time_added.second(),
             self.time_added.date()
-        ));
+        ))
+        .block(Block::bordered().title("Added"));
         let date_edited = Paragraph::new(format!(
-            "edited: {}:{}:{} {}",
+            "{}:{}:{} {}",
             self.time_edited.hour(),
             self.time_edited.minute(),
             self.time_edited.second(),
             self.time_edited.date()
-        ));
+        ))
+        .block(Block::bordered().title("Edited"));
         let chunks = Layout::new(
             Direction::Vertical,
             [Constraint::Percentage(80), Constraint::Percentage(20)],
@@ -51,12 +53,13 @@ impl Widget for Task {
         for (date_widget, time_chunk) in zip([date_added, date_edited], time_chunks.iter()) {
             date_widget.render(*time_chunk, buf);
         }
-        let title = Paragraph::new(self.title.clone());
+        let title = Paragraph::new(self.title.clone()).block(Block::bordered().title("Task"));
         if let Some(desc_text) = self.description {
             let task_chunks =
                 Layout::vertical([Constraint::Percentage(70), Constraint::Percentage(30)])
                     .split(chunks[0]);
-            let description = Paragraph::new(desc_text.clone());
+            let description =
+                Paragraph::new(desc_text.clone()).block(Block::bordered().title("Description"));
             for (task_widget, task_chunk) in zip([title, description], task_chunks.iter()) {
                 task_widget.render(*task_chunk, buf);
             }
