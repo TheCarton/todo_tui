5711dc24ca88917735cfd0bdd1a578b007e5cc14
TheCarton <theukearchy@gmail.com>
2024-09-08 13:41:52 -0400

    status in ui

--- src/task.rs
+++ src/task.rs
@@ -22,8 +22,24 @@ pub enum TaskStatus {
     Finished,
 }
 
+impl TaskStatus {
+    fn to_text(&self) -> String {
+        match self {
+            TaskStatus::InProgress => "In Progress".to_string(),
+            TaskStatus::Finished => "Finished".to_string(),
+        }
+    }
+}
+
 impl Widget for Task {
     fn render(self, area: Rect, buf: &mut Buffer) {
+        let chunks = Layout::new(
+            Direction::Vertical,
+            [Constraint::Percentage(80), Constraint::Percentage(20)],
+        )
+        .split(area);
+        let task_completion =
+            Paragraph::new(self.task_status.to_text()).block(Block::bordered().title("Status"));
         let date_added = Paragraph::new(format!(
             "{}:{}:{} {}",
             self.time_added.hour(),
@@ -40,18 +56,20 @@ impl Widget for Task {
             self.time_edited.date()
         ))
         .block(Block::bordered().title("Edited"));
-        let chunks = Layout::new(
-            Direction::Vertical,
-            [Constraint::Percentage(80), Constraint::Percentage(20)],
-        )
-        .split(area);
-        let time_chunks = Layout::new(
+        let status_chunks = Layout::new(
             Direction::Horizontal,
-            [Constraint::Percentage(50), Constraint::Percentage(50)],
+            [
+                Constraint::Percentage(33),
+                Constraint::Percentage(33),
+                Constraint::Percentage(33),
+            ],
         )
         .split(chunks[1]);
-        for (date_widget, time_chunk) in zip([date_added, date_edited], time_chunks.iter()) {
-            date_widget.render(*time_chunk, buf);
+        for (date_widget, status_chunk) in zip(
+            [task_completion, date_added, date_edited],
+            status_chunks.iter(),
+        ) {
+            date_widget.render(*status_chunk, buf);
         }
         let title = Paragraph::new(self.title.clone()).block(Block::bordered().title("Task"));
         if let Some(desc_text) = self.description {
