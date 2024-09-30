3d9b794e74e4c721a4d9fcc91daa8ab7b9f96e98
TheCarton <theukearchy@gmail.com>
2024-09-30 13:32:52 -0400

    remove option from key input

--- src/input_keys.rs
+++ src/input_keys.rs
@@ -36,91 +36,91 @@ pub fn keycode_to_action(key: KeyCode) -> Option<ActionKind> {
 
 #[derive(Debug, PartialEq, Eq, PartialOrd)]
 pub(crate) struct InputKey {
-    pub(crate) key_code: Option<KeyCode>,
+    pub(crate) key_code: KeyCode,
     pub(crate) name: &'static str,
     pub(crate) description: &'static str,
 }
 
 const ADD_TASK_KEYCODE: KeyCode = KeyCode::Char('a');
 const ADD_TASK_ACTION: ActionKind = ActionKind::AddTask(InputKey {
-    key_code: Some(ADD_TASK_KEYCODE),
+    key_code: ADD_TASK_KEYCODE,
     name: "a",
     description: "add mode",
 });
 
 const EDIT_MODE_KEYCODE: KeyCode = KeyCode::Char('e');
 const EDIT_MODE_ACTION: ActionKind = ActionKind::EditMode(InputKey {
-    key_code: Some(EDIT_MODE_KEYCODE),
+    key_code: EDIT_MODE_KEYCODE,
     name: "e",
     description: "edit mode",
 });
 
 const SHUFFLE_TASK_KEYCODE: KeyCode = KeyCode::Char('r');
 const SHUFFLE_TASK_ACTION: ActionKind = ActionKind::ShuffleTasks(InputKey {
-    key_code: Some(SHUFFLE_TASK_KEYCODE),
+    key_code: SHUFFLE_TASK_KEYCODE,
     name: "r",
     description: "shuffle tasks",
 });
 
 const QUIT_KEYCODE: KeyCode = KeyCode::Char('q');
 const QUIT_ACTION: ActionKind = ActionKind::Quit(InputKey {
-    key_code: Some(QUIT_KEYCODE),
+    key_code: QUIT_KEYCODE,
     name: "q",
     description: "quit",
 });
 
 const MARK_TASK_DONE_KEYCODE: KeyCode = KeyCode::Char('d');
 const MARK_TASK_DONE_ACTION: ActionKind = ActionKind::MarkTaskDone(InputKey {
-    key_code: Some(MARK_TASK_DONE_KEYCODE),
+    key_code: MARK_TASK_DONE_KEYCODE,
     name: "d",
     description: "mark task done",
 });
 
 const MARK_TASK_IN_PROGRESS_KEYCODE: KeyCode = KeyCode::Char('D');
 const MARK_TASK_IN_PROGRESS_ACTION: ActionKind = ActionKind::MarkTaskInProgress(InputKey {
-    key_code: Some(MARK_TASK_IN_PROGRESS_KEYCODE),
+    key_code: MARK_TASK_IN_PROGRESS_KEYCODE,
     name: "D",
     description: "mark task in progress",
 });
 
 const KEYS_HINT_KEYCODE: KeyCode = KeyCode::Char('?');
 const KEYS_HINT_ACTION: ActionKind = ActionKind::KeysHint(InputKey {
-    key_code: Some(KEYS_HINT_KEYCODE),
+    key_code: KEYS_HINT_KEYCODE,
     name: "?",
     description: "mark task in progress",
 });
 
 const FOCUS_TITLE_KEYCODE: KeyCode = KeyCode::Char('t');
 const FOCUS_TITLE_ACTION: ActionKind = ActionKind::FocusTitle(InputKey {
-    key_code: Some(FOCUS_TITLE_KEYCODE),
+    key_code: FOCUS_TITLE_KEYCODE,
     name: "a",
     description: "focus title input field",
 });
 
 const FOCUS_DESCRIPTION_KEYCODE: KeyCode = KeyCode::Char('T');
 const FOCUS_DESCRIPTION_ACTION: ActionKind = ActionKind::FocusDescription(InputKey {
-    key_code: Some(FOCUS_DESCRIPTION_KEYCODE),
+    key_code: FOCUS_DESCRIPTION_KEYCODE,
     name: "d",
     description: "focus title description field",
 });
 
 const CHANGE_MODE_KEYCODE: KeyCode = KeyCode::Esc;
 const CHANGE_MODE_ACTION: ActionKind = ActionKind::ChangeMode(InputKey {
-    key_code: Some(CHANGE_MODE_KEYCODE),
+    key_code: CHANGE_MODE_KEYCODE,
     name: "Escape",
     description: "change modes",
 });
 
 const INCREMENT_DUE_DATE_BY_1_KEYCODE: KeyCode = KeyCode::Char('y');
 const INCREMENT_DUE_DATE_BY_1_ACTION: ActionKind = ActionKind::IncrementDueDate(InputKey {
-    key_code: Some(INCREMENT_DUE_DATE_BY_1_KEYCODE),
+    key_code: INCREMENT_DUE_DATE_BY_1_KEYCODE,
     name: "y",
     description: "increment due date",
 });
 
 const DECREMENT_DUE_DATE_BY_1_KEYCODE: KeyCode = KeyCode::Char('Y');
 const DECREMENT_DUE_DATE_BY_1_ACTION: ActionKind = ActionKind::DecrementDueDate(InputKey {
-    key_code: Some(DECREMENT_DUE_DATE_BY_1_KEYCODE),
+    key_code: DECREMENT_DUE_DATE_BY_1_KEYCODE,
     name: "Y",
     description: "decrement due date by 1",
 });
